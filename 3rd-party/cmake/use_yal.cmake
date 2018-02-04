fdf8087408da55b0f1a67d7427d0ff23d68e0ec1
mtrempoltsev <mt@devexp.ru>
2018-02-04 16:01:50 +0300

    back to work

--- 3rd-party/cmake/use_yal.cmake
+++ 3rd-party/cmake/use_yal.cmake
@@ -4,6 +4,7 @@
         set(YAL_BUILD_GTEST     OFF CACHE BOOL "Build Gtest" FORCE)
         set(YAL_BUILD_TESTS     OFF CACHE BOOL "Build YAL tests" FORCE)
         set(YAL_BUILD_EXAMPLES  OFF CACHE BOOL "Build YAL examples" FORCE)
+
         add_subdirectory("${3rd_parties_root}/yal" yal)
         include_directories("${3rd_parties_root}/yal/include")
     endif()
